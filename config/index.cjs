61bf6d3d945f02e09d9de35daae56d01b23c82f1
13428939796 <115863790+13428939796@users.noreply.github.com>
2022-10-15 15:25:04 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: '123',
+      name: '今日小推送',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ozofm5ubQSaOSAXV1jp1AXT-EdJs',
     }

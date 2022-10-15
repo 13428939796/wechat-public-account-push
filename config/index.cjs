60826fae37f2f4ab4067579ea99b1dff61805eaf
13428939796 <115863790+13428939796@users.noreply.github.com>
2022-10-15 15:33:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,13 +21,16 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '洪洪',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ozofm5ubQSaOSAXV1jp1AXT-EdJs',
+      id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      'MQ3swmbBGnY0qo2jUmTz0XzKzNWjjFfEXuMzmth-2Sk',
+      useTemplateId: 'ozofm5ubQSaOSAXV1jp1AXT-EdJs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '06-17',
+      horoscopeDate: '12-27',
       festivals: [
-        
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '洪洪', year: '2004', date: '06-17',
@@ -40,7 +43,8 @@ const USER_CONFIG = {
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2020-07-30' },
-        
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
@@ -51,13 +55,10 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: '今日小推送',
+      name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ozofm5ubQSaOSAXV1jp1AXT-EdJs',
     }
   ],
 
 }
-
-module.exports = USER_CONFIG
-
